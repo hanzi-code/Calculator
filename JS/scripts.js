53da4b40027b899c9322c1bcfefcf8bd3f296f06
hanzi-code <195461519+hanzi-code@users.noreply.github.com>
2025-03-11 10:08:54 -0300

    Edit scripts.js
    Add the logic for the number buttons, dot btn,
    the delete btn and all clear btn

--- JS/scripts.js
+++ JS/scripts.js
@@ -7,3 +7,22 @@ document.querySelector("#backspace").addEventListener('click', () => {
 document.querySelector("#allClear").addEventListener('click', () => {
     inputField.value = '';
 });
+
+document.querySelectorAll(".number").forEach((button) => {
+    button.addEventListener('click', () => {
+        appendValue(button.textContent);
+    });
+});
+
+document.querySelector("#point").addEventListener('click', () => {
+    const currentValue = inputField.value;
+    if (currentValue.includes('.')) {
+        console.log("No more dots for you!")
+    } else {
+        appendValue('.');
+    }
+});
+
+function appendValue(value) {
+    inputField.value += value;
+}
